2bbd91e0ecb2a135dd31a7a6a1291ec62c524ac2
Ameer Badushah T <84665215+ameerbadushaht@users.noreply.github.com>
2023-08-22 22:08:46 +0530

    Update favourite

--- server/src/routers/favorite.js
+++ server/src/routers/favorite.js
@@ -4,29 +4,6 @@ import { ProductModel } from '../models/Product.js';
 import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
-
-// Add a product to the user's favorites
-// router.post('/add', async (req, res) => {
-//   const { userID, productID } = req.body;
-  
-//   try {
-
-//       const newFavorite = new FavModel(
-//         { 
-//           userID, productID 
-//         }
-//         );
-//         await newFavorite.save();
-//         res.json("Added to wishlist")
-
-//     }
-//  catch (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-// });
-
-
-
 //test add fav
 
 
@@ -67,58 +44,5 @@ router.get('/saved/:userID', async (req, res) => {
     res.json(err);
   }
 });
-
-
-
-
-
-
-
-
-
-
-// router.get('/show',async(req, res)=>{
-//   try {
-
-
-//     const favProduct = await FavModel.find({});
-//     if(favProduct){
-//       res.json(favProduct);
-
-
-//     }
-    
-// } catch (error) {
-//     res.json({ error: error.message });
-// }
-// })
-
-
-
-
-
-
-
-
-
-
-// router.get('/show', async (req, res) => {
-//   try {
-//     const favProducts = await FavModel.find({}, { userID: 1, _id: 0 }); // Projection to include only userID field
-//     if (favProducts.length > 0) {
-//       const userIDs = favProducts.map((favProduct) => favProduct.userID);
-
-//       // Find products based on userIDs
-//       const products = await ProductModel.find({ userID: { $in: userIDs } });
-
-//       res.json(products);
-//     } else {
-//       res.status(404).json({ message: 'No favorite products found.' });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
 
 export  {router as favRouter};
