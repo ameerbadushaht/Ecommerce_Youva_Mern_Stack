4976a5811e13bd91af1238b94b28398ae975f7a4
Ameer Badushah T <84665215+ameerbadushaht@users.noreply.github.com>
2023-08-28 23:00:15 +0530

    Update cart.js

--- server/src/routers/cart.js
+++ server/src/routers/cart.js
@@ -3,38 +3,113 @@ import { CartModel } from "../models/cart.js";
 
 const router = express.Router();
 
+router.get('/show',async(req,res)=>{
+  const { userID} = req.body;
+
+  try{
+    const cart = await CartModel.find({userID})
+ if(cart){
+ res.json(cart)
+ }
+    
+  }
+  catch (error) {
+    res.json({ error: error.message });
+}
+})
+
 router.post("/add", async (req, res) => {
-  const { userID } = req.body;
-  const { productID } = req.body.cartItems.productID;
+  const { userID, cartItems } = req.body;
+  const productID = cartItems.productID;
+  const quantity = cartItems.quantity;
 
-  const user = await CartModel.findOne({ userID });
-  if (user) {
-    try {
-      const product = await CartModel.findOne({ productID });
+  try {
+    const user = await CartModel.findOne({ userID });
+
+    if (user) {
+      const product = await CartModel.findOne({
+        userID: user.userID,  // Ensure the product is within the user's cart
+        "cartItems.productID": productID,
+      });
+
       if (product) {
-        
-      }else{
-        return res.json({ message: "Product not found" });
+        await CartModel.findOneAndUpdate(
+          { userID, "cartItems.productID": productID },  // Update specific user's cart
+          {
+            $inc: { "cartItems.$.quantity": quantity },  // Increment quantity
+          }
+        );
+
+        res.json({ message: "success quantity increased" });
+      } else {
+        await CartModel.findOneAndUpdate(
+          { userID },
+          {
+            $push: {
+              cartItems: cartItems,
+            },
+          }
+        );
+
+        res.json({ message: "product added" });
       }
-
-      // CartModel.findOneAndUpdate({ userID},{
-      //   "$push":{"cartItems":req.body.cartItems}})
-
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  } else {
-    try {
+    } else {
       const cart = new CartModel({
         userID,
-        cartItems: [req.body.cartItems],
+        cartItems: [cartItems],
       });
-      cart.save();
+
+      await cart.save();
       res.status(201).json(cart);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+     
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
+
+
+router.post('/delete',async (req, res) => {
+  const { userID, cartItems } = req.body;
+  const productID = cartItems.productID;
+  const quantity = cartItems.quantity;
+  try {
+    const user = await CartModel.findOne({ userID });
+
+    if (user) {
+      const product = await CartModel.findOne({
+        userID: user.userID,  // Ensure the product is within the user's cart
+        "cartItems.productID": productID,
+      });
+
+      if (product) {
+        await CartModel.findOneAndUpdate(
+          { userID, "cartItems.productID": productID },  // Update specific user's cart
+          {
+            $inc: { "cartItems.$.quantity": -quantity },  // Increment quantity
+          }
+        );
+
+        res.json({ message: "success quantity decreased" });
+      } else {
+        await CartModel.findOneAndUpdate(
+          { userID },
+          {
+            $push: {
+              cartItems: cartItems,
+            },
+          }
+        );
+
+        res.json({ message: "product added" });
+      }
+    } else {
+      res.json({ message: "No user Found" });
+     
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 export { router as cartRouter };
