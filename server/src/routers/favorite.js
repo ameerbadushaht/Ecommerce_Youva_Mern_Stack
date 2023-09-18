98618d29ee310c8e28018f93f7e02eb37bd2f083
Ameer Badushah T <84665215+ameerbadushaht@users.noreply.github.com>
2023-09-18 08:33:33 +0530

    Update favorite.js

--- server/src/routers/favorite.js
+++ server/src/routers/favorite.js
@@ -1,48 +1,43 @@
-import express from 'express';
-import { FavModel } from '../models/Favorite.js';
-import { ProductModel } from '../models/Product.js';
-import { UserModel } from '../models/Users.js';
+import express from "express";
+import { FavModel } from "../models/Favorite.js";
+import { ProductModel } from "../models/Product.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
-//test add fav
 
-
-router.post('/add', async (req, res) => {
+router.post("/add", async (req, res) => {
   const { userID, productID } = req.body;
-try{
-  const user = await UserModel.findById(userID);
-  const product = await ProductModel.findById(productID);
-  user.fav.push(product);
-  await user.save();
-  res.json({fav:user.fav})
-
-
-}catch(err){
-  res.json(err);
-}
-
-})
-
-router.get('/fav', async (req, res) => {
-  try{
-    const user =await UserModel.findById(req.params.userID);
-    res.json({fav: user?.fav});
-  }catch(err){
+  try {
+    const user = await UserModel.findById(userID);
+    const product = await ProductModel.findById(productID);
+    user.fav.push(product);
+    await user.save();
+    res.json({ fav: user.fav });
+  } catch (err) {
     res.json(err);
   }
 });
 
-router.get('/saved/:userID', async (req, res) => {
-  try{
-    const user =await UserModel.findById(req.params.userID);
+router.get("/fav", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userID);
+    res.json({ fav: user?.fav });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/saved/:userID", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userID);
     const favorite = await ProductModel.find({
-      _id:{ $in : user.fav },
-    })
-    
-    res.json({favorite});
-  }catch(err){
+      _id: { $in: user.fav },
+    });
+
+    res.json({ favorite });
+  } catch (err) {
     res.json(err);
   }
 });
 
-export  {router as favRouter};
+export { router as favRouter };
