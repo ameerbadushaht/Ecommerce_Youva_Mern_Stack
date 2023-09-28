8a96974dd3d359490e978cc3f858dc2f3ae8143a
Ameer Badushah T <84665215+ameerbadushaht@users.noreply.github.com>
2023-09-28 15:43:02 +0530

    Update Products.jsx

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,22 +1,129 @@
 import styled from "styled-components";
-import { popularProducts } from "../data";
-import Product from "./Product";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+
+//All product shows here
+
+const Container1 = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
+
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/products/list/");
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+ 
+  const navigate = useNavigate();
+  
+const handleButton = (productId) => {
+  navigate(`/eachproduct/${productId}`);
+};
+
   return (
-    <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
+    <Container1 >
+      {products.map((product) => (
+        <Container id={product._id} key={product._id} onClick={()=>handleButton(product._id)}> 
+          <Circle />
+          <Image src={product.imageUrl} />
+          <Info>
+            <Icon>
+              <ShoppingCartIcon />
+            </Icon>
+            {/* <Icon onClick={() => favoriteProduct(product._id)}>
+              {clickedProducts[product._id] ? (
+                <FavoriteIcon />
+              ) : (
+                <FavoriteBorderIcon />
+              )}
+            </Icon> */}
+          </Info>
+        </Container>
       ))}
-    </Container>
+    </Container1>
   );
 };
+
+
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
 
 export default Products;
